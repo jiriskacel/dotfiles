72ea30d51fed22b1174c0b7826bc6400f6571374
Jiri Skacel <jiri.skacel@solarwinds.com>
2021-03-01 18:18:07 +0100

    update bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,3 +1,3 @@
 source ~/.dotfiles/git/git-prompt.sh
 
-PROMPT_COMMAND='__posh_git_ps1 "\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] " "\\\$ ";'$PROMPT_COMMAND
+PROMPT_COMMAND='__posh_git_ps1 "\[\033[00;90m\]\u\[\033[00m\]@\[\033[36m\]\h\[\033[00m\]:\[\033[34m\]\w\[\033[00m\] " "\\\$ ";'$PROMPT_COMMAND
