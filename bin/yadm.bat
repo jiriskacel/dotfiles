c2bd9c8ab964cff9f8b5aa0d423a7f52b46ba2c6
Jiří Skácel <jiriskacel@seznam.cz>
2024-02-07 23:25:10 +0100

    remove windows-specific settings

--- bin/yadm.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for %%I in ("git.exe") do for %%J in ("%%~$PATH:I") do set GitFolder=%%~dpJ..\
-set ShellExe=%GitFolder%bin\sh.exe
-set GIT_WORK_TREE=%USERPROFILE%
-
-"%ShellExe%" %~dp0yadm %*
