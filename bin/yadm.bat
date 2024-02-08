90fc8a4cca1d4e76a7ace60d84808854219d224e
Jiří Skácel <jiriskacel@seznam.cz>
2024-02-08 16:13:28 +0100

    keep GitFolder when defined in parent environment

--- bin/yadm.bat
+++ bin/yadm.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for %%I in ("git.exe") do for %%J in ("%%~$PATH:I") do set GitFolder=%%~dpJ..\
+if "%GitFolder%" == "" for %%I in ("git.exe") do for %%J in ("%%~$PATH:I") do set GitFolder=%%~dpJ..\
 set ShellExe=%GitFolder%bin\sh.exe
 set GIT_WORK_TREE=%USERPROFILE%
 
