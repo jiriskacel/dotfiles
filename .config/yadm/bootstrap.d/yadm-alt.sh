f70aac04dfa6f336c5437e332ac02735f83b6ae3
Jiří Skácel <jiriskacel@seznam.cz>
2024-05-30 17:15:43 +0200

    move alias.alt-back to yadm only

--- .config/yadm/bootstrap.d/yadm-alt.sh
+++ .config/yadm/bootstrap.d/yadm-alt.sh
@@ -7,6 +7,7 @@ if [ -n "$WINDIR" ]; then
 
     $YADM config yadm.auto-alt false
     $YADM config yadm.alt-copy true
+    $YADM gitconfig alias.alt-back '!f() { cp() { git cat-file blob :.${2#$PWD} > $2.BASE; git merge-file -L common -L base -L local $2 $2.BASE $3; [ $? -gt 0 ] && echo ""There were conflits on $2"" >&2; rm -f $2.BASE; }; export -f cp; yadm alt >/dev/null; }; f'
 
     echo "✔  $SCRIPT"
 fi
