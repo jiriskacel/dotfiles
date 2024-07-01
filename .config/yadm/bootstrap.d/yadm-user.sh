1e150d6a8592a0c7121871c82bc48b0ef2615ef3
Jiří Skácel <jiriskacel@seznam.cz>
2024-07-01 10:50:01 +0200

    force set username for remote.origin.url

--- .config/yadm/bootstrap.d/yadm-user.sh
+++ .config/yadm/bootstrap.d/yadm-user.sh
@@ -6,6 +6,7 @@ echo "⚡ $SCRIPT"
 
 $YADM gitconfig user.name "Jiří Skácel"
 $YADM gitconfig user.email "jiriskacel@seznam.cz"
+$YADM gitconfig remote.origin.url "https://jiriskacel@github.com/jiriskacel/dotfiles"
 $YADM gitconfig commit.gpgSign false
 
 echo "✔  $SCRIPT"
